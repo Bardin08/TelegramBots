87e45cb046bd7b0fc3a012489ee43272d0699ec3
bardin08 <asdasfb326@gmail.com>
2020-11-09 10:36:17 +0200

    ADD dependency injection, logging and configuration

---
+++ BotController/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace BotController
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .CreateLogger();
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    // ... Register services here ...
+                });
+
+            // ... Create and execute here ...
+        }
+
+        private static void BuildConfig(ConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}
