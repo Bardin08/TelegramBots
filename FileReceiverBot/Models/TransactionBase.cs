5ee62af0f695604b258b6e6bf461c09dbc119b48
bardin08 <asdasfb326@gmail.com>
2020-11-09 12:01:01 +0200

    ADD transaction base model

---
+++ FileReceiverBot/Models/TransactionBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace FileReceiverBot.Models
+{
+    public class TransactionBase
+    {
+        public int TransactionId { get; set; }
+        public int RecepientId { get; set; }
+        public string TransactionType { get; set; }
+        public List<int> MessageIds { get; set; }
+    }
+}
