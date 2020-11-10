ef583ccb4bb5771cf82b6dfc636ba608435008a1
bardin08 <asdasfb326@gmail.com>
2020-11-10 17:00:28 +0200

    ADD start command

---
+++ FileReceiverBot/Commands/StartCommand.cs
@@ -0,0 +1,18 @@
+﻿using FileReceiverBot.Interfaces;
+using FileReceiverBot.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace FileReceiverBot.Commands
+{
+    public class StartCommand : IFileReceiverBotCommand
+    {
+        public string Name => "/start";
+
+        public async void Execute(Message message, CommandTransaction transaction, ITelegramBotClient botClient)
+        {
+            await botClient.SendTextMessageAsync((transaction as CommandTransaction).RecepientId, "Этот бот предназначен для сохранения файлов. Для отправки файлов воспользуйся командой /send");
+            (transaction as CommandTransaction).IsComplete = true;
+        }
+    }
+}
