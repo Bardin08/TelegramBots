1223a4fa4a674e65668cc3de9feca0f0dc17e575
bardin08 <asdasfb326@gmail.com>
2020-11-10 17:01:43 +0200

    ADD bot command interface

---
+++ FileReceiverBot/Interfaces/IFileReceiverBotCommand.cs
@@ -0,0 +1,13 @@
+﻿using FileReceiverBot.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace FileReceiverBot.Interfaces
+{
+    public interface IFileReceiverBotCommand
+    {
+        public string Name { get; }
+
+        public void Execute(Message message, CommandTransaction transaction, ITelegramBotClient botClient);
+    }
+}
