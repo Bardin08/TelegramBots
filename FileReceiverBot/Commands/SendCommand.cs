086a11aff2f3586a9f5d29ce47feb59773f1799a
bardin08 <asdasfb326@gmail.com>
2020-11-10 17:01:16 +0200

    ADD send command

---
+++ FileReceiverBot/Commands/SendCommand.cs
@@ -0,0 +1,21 @@
+﻿using FileReceiverBot.Interfaces;
+using FileReceiverBot.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace FileReceiverBot.Commands
+{
+    public class SendCommand : IFileReceiverBotCommand
+    {
+        public delegate void FileReceivingTransactionEvent(FileReceivingTransaction transaction);
+        public static event FileReceivingTransactionEvent TransactionInitiated;
+
+        public string Name => "/send";
+
+        public void Execute(Message message, CommandTransaction transaction, ITelegramBotClient botClient)
+        {
+            transaction.IsComplete = true;
+            TransactionInitiated?.Invoke(new FileReceivingTransaction(transaction.RecepientId));
+        }
+    }
+}
