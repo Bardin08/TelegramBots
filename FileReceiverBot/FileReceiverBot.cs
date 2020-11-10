8eea36df5175512082cb64897364d49663ff5d51
bardin08 <asdasfb326@gmail.com>
2020-11-10 17:03:19 +0200

    UPDATE bot main class

--- FileReceiverBot/FileReceiverBot.cs
+++ FileReceiverBot/FileReceiverBot.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading;
+using FileReceiverBot.Commands;
 using FileReceiverBot.Interfaces;
 using FileReceiverBot.Models;
+using FileReceiverBot.TransactionProcessStrategies;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 using Telegram.Bot.Args;
@@ -14,6 +17,8 @@ namespace FileReceiverBot
         private readonly ITelegramBotClient _botClient;
         private readonly ILogger<FileReceiverBot> _logger;
 
+        private readonly TransactionsProcessor _transactionsProcessor;
+
         private List<object> _transactions;
 
         public FileReceiverBot(IFileReceiverBotClient botClient, ILogger<FileReceiverBot> logger)
@@ -22,6 +27,7 @@ namespace FileReceiverBot
             _logger = logger;
 
             _transactions = new List<object>();
+            _transactionsProcessor = new TransactionsProcessor();
         }
 
         public void Execute()
@@ -31,8 +37,21 @@ namespace FileReceiverBot
             _botClient.OnMessage += MessageReceived;
             _botClient.OnCallbackQuery += CallbackQueryReceived;
 
+            SendCommand.TransactionInitiated += FileReceivingTransactionInitiated;
+
             _botClient.StartReceiving();
             Thread.Sleep(int.MaxValue);
+        }
+
+        private void FileReceivingTransactionInitiated(FileReceivingTransaction transaction)
+        {
+            if (transaction != null)
+            {
+                _transactions.Add(transaction);
+            }
+
+            _transactionsProcessor.ProcessStrategy = new FileReceiptProcessingStrategy();
+            _transactionsProcessor.Process(new Message(), transaction, _botClient);
         }
 
         private void CallbackQueryReceived(object sender, CallbackQueryEventArgs e)
@@ -55,29 +74,29 @@ namespace FileReceiverBot
         {
             object userTransaction = _transactions.Find(transaction => (transaction as TransactionBase)?.RecepientId == message.From.Id);
 
-            if (message.Text == "/send")
+            if (message.Text?.StartsWith("/") == true)
             {
                 if (userTransaction == null)
                 {
-                    userTransaction = new FileReceivingTransaction(message.From.Id);
-                    _transactions.Add(userTransaction);
+                    _transactionsProcessor.ProcessStrategy = new CommandProcessingStrategy();
+                    userTransaction = new CommandTransaction(message.From.Id);
                 }
             }
-
-            if ((userTransaction as TransactionBase)?.TransactionType == "FileReceiving")
+            else if ((userTransaction as TransactionBase)?.TransactionType == "FileReceiving")
             {
-                var tp = new TransactionsProcessor
-                {
-                    ProcessStrategy = new TransactionProcessStrategies.FileReceiptProcessingStrategy()
-                };
+                _transactionsProcessor.ProcessStrategy = new FileReceiptProcessingStrategy();
+            }
 
-                tp.Process(message, userTransaction, _botClient);
+
+            if (userTransaction != null)
+            {
+                _transactionsProcessor.Process(message, userTransaction, _botClient);
             }
         }
 
         private void DeleteCompleteTransactions()
         {
-            _transactions.RemoveAll(x => (x as TransactionBase).IsComplete);
+            _transactions.RemoveAll(x => (x as TransactionBase)?.IsComplete ?? false);
         }
     }
 }
