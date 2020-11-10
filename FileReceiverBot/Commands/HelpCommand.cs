3c929a861948be74c9c165e5c3da19cc3705023d
bardin08 <asdasfb326@gmail.com>
2020-11-10 17:01:04 +0200

    ADD help command

---
+++ FileReceiverBot/Commands/HelpCommand.cs
@@ -0,0 +1,18 @@
+﻿using FileReceiverBot.Interfaces;
+using FileReceiverBot.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace FileReceiverBot.Commands
+{
+    public class HelpCommand : IFileReceiverBotCommand
+    {
+        public string Name => "/help";
+
+        public async void Execute(Message message, CommandTransaction transaction, ITelegramBotClient botClient)
+        {
+            await botClient.SendTextMessageAsync(transaction.RecepientId, "Help text here!");
+            transaction.IsComplete = true;
+        }
+    }
+}
