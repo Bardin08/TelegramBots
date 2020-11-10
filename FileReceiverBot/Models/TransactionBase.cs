5390eee41459971596bfbd4714105e3ccfd8cabe
bardin08 <asdasfb326@gmail.com>
2020-11-10 08:14:56 +0200

    UPDATE transaction base model

--- FileReceiverBot/Models/TransactionBase.cs
+++ FileReceiverBot/Models/TransactionBase.cs
@@ -4,6 +4,12 @@ namespace FileReceiverBot.Models
 {
     public class TransactionBase
     {
+        public TransactionBase()
+        {
+            MessageIds = new List<int>();
+        }
+
+        public bool IsComplete { get; set; }
         public int TransactionId { get; set; }
         public int RecepientId { get; set; }
         public string TransactionType { get; set; }
