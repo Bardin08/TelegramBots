3b196dbbe17a524d47ead2b59693948b54a78b72
bardin08 <asdasfb326@gmail.com>
2020-11-10 17:00:54 +0200

    ADD is on command
    Shows if bot is on

---
+++ FileReceiverBot/Commands/IsOnCommand.cs
@@ -0,0 +1,17 @@
+﻿using FileReceiverBot.Interfaces;
+using FileReceiverBot.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace FileReceiverBot.Commands
+{
+    class IsOnCommand : IFileReceiverBotCommand
+    {
+        public string Name => "/ison";
+
+        public async void Execute(Message message, CommandTransaction transaction, ITelegramBotClient botClient)
+        {
+            await botClient.SendTextMessageAsync(transaction.RecepientId, "Бот включен");
+        }
+    }
+}
