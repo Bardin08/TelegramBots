b5df0a8de718741dc8d60fc57d2b241252dc3e40
bardin08 <asdasfb326@gmail.com>
2020-12-20 23:31:43 +0200

    Add hash code and comparing methods to TransactionBase model

--- FileReceiverBot/Common/Models/TransactionBase.cs
+++ FileReceiverBot/Common/Models/TransactionBase.cs
@@ -12,7 +12,21 @@ namespace FileReceiverBot.Common.Models
         public bool IsComplete { get; set; }
         public int TransactionId { get; set; }
         public int RecepientId { get; set; }
+        public string Username { get; set; }
         public string TransactionType { get; set; }
         public List<int> MessageIds { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (this == obj) return true;
+            if (((TransactionBase)obj).TransactionId == this.TransactionId) return true;
+
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return TransactionId.GetHashCode() * 31;
+        }
     }
 }
