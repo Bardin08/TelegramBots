be2737b19cd2cf0a4955039291605068d9a8e39e
bardin08 <asdasfb326@gmail.com>
2020-12-26 10:36:18 +0200

    Add internal bot exception

---
+++ FileReceiverBot/Common/Exceptions/InternalBotErrorException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FileReceiverBot.Common.Exceptions
+{
+    public class InternalBotErrorException : Exception
+    {
+        public InternalBotErrorException() : base()
+        {
+        }
+
+        public InternalBotErrorException(string message) : base(message)
+        {
+        }
+
+        public InternalBotErrorException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
