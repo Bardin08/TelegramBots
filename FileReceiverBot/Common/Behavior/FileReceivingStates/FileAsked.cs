226367d734be0bdd1c40b22a660b1df71193ac1c
bardin08 <asdasfb326@gmail.com>
2020-12-26 09:56:29 +0200

    Remove adding to log information about sent message

--- FileReceiverBot/Common/Behavior/FileReceivingStates/FileAsked.cs
+++ FileReceiverBot/Common/Behavior/FileReceivingStates/FileAsked.cs
@@ -12,16 +12,9 @@ namespace FileReceiverBot.Common.Behavior.FileReceivingStates
     {
         public async Task ProcessTransactionAsync(Message message, FileReceivingTransaction transaction, ITelegramBotClient botClient, ILogger logger)
         {
-            logger.LogDebug("File request initialized for user {username}({id})", transaction.Username, transaction.RecepientId);
-
             try
             {
-                var sentMessage = await botClient.SendTextMessageAsync(transaction.RecepientId, "Отправь мне файл, который нужно сохранить.");
-
-                if (sentMessage != null)
-                {
-                    logger.LogDebug("File request sent to {username}({id})", transaction.Username, transaction.RecepientId);
-                }
+                _ = await botClient.SendTextMessageAsync(transaction.RecepientId, "Отправь мне файл, который нужно сохранить.");
             }
             catch (Exception ex)
             {
