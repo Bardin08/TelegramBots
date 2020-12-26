8b7660da47e194203f5ffc4f94f13dccffb6022d
bardin08 <asdasfb326@gmail.com>
2020-12-26 21:26:44 +0200

    Add file saving path as a constant

--- FileReceiverBot/BotConstants.cs
+++ FileReceiverBot/BotConstants.cs
@@ -3,5 +3,6 @@
     internal static class BotConstants
     {
         public static string LabelsFileFullName => @"C:\Users\User\Desktop\IT01.Telegram.Bots\ActiveLabels.txt";
+        public static string FileSavingPath => @"C:\Users\User\Desktop\IT01.Telegram.Bots\Received\Files\";
     }
 }
