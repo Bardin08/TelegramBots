df56bf0ded018f03a554c41b40b40d27eea7840c
bardin08 <asdasfb326@gmail.com>
2020-12-26 21:26:15 +0200

    Add issaved command

---
+++ FileReceiverBot/Commands/IsSavedCommand.cs
@@ -0,0 +1,20 @@
+﻿using FileReceiverBot.Common.Interfaces;
+using FileReceiverBot.Common.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace FileReceiverBot.Commands
+{
+    internal class IsSavedCommand : IFileReceiverBotCommand
+    {
+        public delegate void FileCheckTransactionEvent(FileCheckTransaction transaction);
+        public static event FileCheckTransactionEvent FileCheckTransactionInitiated;
+
+        public string Name => "/issaved";
+
+        public void Execute(Message message, CommandTransaction transaction, ITelegramBotClient botClient)
+        {
+            FileCheckTransactionInitiated?.Invoke(new FileCheckTransaction(message.From.Id));
+        }
+    }
+}
