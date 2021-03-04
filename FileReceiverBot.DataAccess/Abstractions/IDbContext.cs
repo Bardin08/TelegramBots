7ed1fb7579477b9aac90f2b3c8417e127d3e7870
bardin08 <asdasfb326@gmail.com>
2021-03-04 09:06:19 +0200

    Create bot api project

---
+++ FileReceiverBot.DataAccess/Abstractions/IDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace FileReceiverBot.DataAccess.Abstractions
+{
+    public interface IDbContext : IDisposable
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        void Update<TEntity>(TEntity entity) where TEntity : class;
+        Task<int> SaveChangesAsync();
+    }
+}
